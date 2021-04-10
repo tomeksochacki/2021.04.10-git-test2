a8f5a3595be5490721bdd6976302620ed44b3e02
tomek <tomeksochacki@wp.pl>
2021-04-10 09:48:08 +0200

    Commit

---
+++ src/pl/camp/it/Klasa.java
@@ -0,0 +1,13 @@
+package pl.camp.it;
+
+class Klasa {
+    private int id;
+
+    int getId() {
+        return id;
+    }
+
+    void setId(final int id) {
+        this.id = id;
+    }
+}
--- src/pl/camp/it/Main.java
+++ src/pl/camp/it/Main.java
@@ -1,4 +1,7 @@
 package pl.camp.it;
 
 class Main {
+    public static void main(String[] args) {
+
+    }
 }
